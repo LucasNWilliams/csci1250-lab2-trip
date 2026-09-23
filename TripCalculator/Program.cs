85d6006e4592d753fbe687585d06f7d199d4a742
Lucas Williams <lucasnwilliams2008@gmail.com>
2026-09-23 13:03:46 -0400

    Fix data types

--- TripCalculator/Program.cs
+++ TripCalculator/Program.cs
@@ -8,16 +8,16 @@ Console.WriteLine("How many miles is the round trip?");
 tripTotalMiles = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("What is the miles per gallon of the car?");
-milesPerGallon = Convert.ToInt32(Console.ReadLine());
+milesPerGallon = Convert.ToDouble(Console.ReadLine());
 
 Console.WriteLine("What is the price per gallon?");
-pricePerGallon = Convert.ToInt32(Console.ReadLine());
+pricePerGallon = Convert.ToDecimal(Console.ReadLine());
 
 // Calculate fuel costs
 double gallonsNeeded = tripTotalMiles / milesPerGallon;
 decimal fuelCost = (decimal)gallonsNeeded * pricePerGallon;
 
 // Tell user the gallons needed and the cost
-Console.WriteLine("Gallons needed: " + gallonsNeeded.ToString("F2") + "(9.29)");
+Console.WriteLine("Gallons Needed: " + gallonsNeeded.ToString("F2") + "(9.29)");
 Console.WriteLine("Fuel Cost: " + fuelCost.ToString("C") + "($26.84)");
 
